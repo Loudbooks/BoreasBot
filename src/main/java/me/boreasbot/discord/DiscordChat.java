c8abb6b5e0a116543db4f5a01bad2c7f7ecc82d2
Loudbook <loudbooked@gmail.com>
2022-05-16 16:32:11 -0400

    Fixed my terrible Discord -> Minecraft code

--- src/main/java/me/boreasbot/discord/DiscordChat.java
+++ src/main/java/me/boreasbot/discord/DiscordChat.java
@@ -1,23 +1,35 @@
 package me.boreasbot.discord;
 
+import com.github.steveice10.mc.protocol.packet.ingame.clientbound.ClientboundLoginPacket;
+import com.github.steveice10.mc.protocol.packet.ingame.serverbound.ServerboundChatPacket;
+import com.github.steveice10.packetlib.Session;
+import com.github.steveice10.packetlib.event.session.SessionAdapter;
+import com.github.steveice10.packetlib.packet.Packet;
+import com.google.gson.JsonObject;
 import net.dv8tion.jda.api.entities.ChannelType;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-import static me.boreasbot.discord.Main.hashMap;
+import static me.boreasbot.discord.Main.*;
 
 
-public class DiscordChat extends ListenerAdapter{
-
+public class DiscordChat extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent e) {
-
         if (e.getAuthor().isBot()) return;
         if (e.getChannelType() == ChannelType.TEXT && e.getTextChannel().getId().equals("872232416771735592")) {
+            Member member = e.getGuild().getMember(e.getAuthor());
+            JsonObject jsonObject = MongoDBUtil.readData("_id", member.getId());
             String author = e.getAuthor().getName();
+            if (jsonObject.get("donator") != null || jsonObject.get("role") != null){
+                author = member.getNickname();
+            }
             String message = e.getMessage().getContentDisplay();
             if (!(message.chars().count() > 255)){
-                hashMap.add("/gc " + author + ": " + message);
+                client.send(new ServerboundChatPacket("/gc " + author + ": " + message));
+                messages.add(e.getTextChannel().getId());
+                messagesID.add(e.getMessage().getId());
             } else {
                 e.getMessage().reply("Message is too long, canceling send.").queue();
             }
@@ -25,7 +37,9 @@ public class DiscordChat extends ListenerAdapter{
             String author = e.getAuthor().getName();
             String message = e.getMessage().getContentDisplay();
             if (!(message.chars().count() > 255)){
-                hashMap.add("/go " + author + ": " + message);
+                client.send(new ServerboundChatPacket("/go " + author + ": " + message));
+                messages.add(e.getTextChannel().getId());
+                messagesID.add(e.getMessage().getId());
             } else {
                 e.getMessage().reply("Message is too long, canceling send.").queue();
             }
